fb0a86bea86eb41df82b00f4ccd7b1f5fba3ef0f
isletspace <liewzheng@qq.com>
2026-09-09 14:50:25 +0800

    chore(branding): point user-facing repo references to Liewzheng/planebot (PLANE-26)

--- apps/web/core/components/power-k/config/help-commands.ts
+++ apps/web/core/components/power-k/config/help-commands.ts
@@ -63,7 +63,7 @@ export const usePowerKHelpCommands = (): TPowerKCommandConfig[] => {
       i18n_title: "power_k.help_actions.report_bug",
       icon: Github,
       action: () => {
-        window.open("https://github.com/makeplane/plane/issues/new/choose", "_blank", "noopener,noreferrer");
+        window.open("https://github.com/Liewzheng/planebot/issues/new/choose", "_blank", "noopener,noreferrer");
       },
       isEnabled: () => true,
       isVisible: () => true,
